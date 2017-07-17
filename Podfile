a64e02bffb599ae4c61365005e4c1f436e02156b
zhaoyunpeng <807536782@qq.com>
2017-07-17 20:24:10 +0800

    init

---
+++ Podfile
@@ -0,0 +1,8 @@
+project "ZYP_CoreDraw.xcodeproj"
+
+platform:ios,'10.0'
+inhibit_all_warnings!
+target 'ZYP_CoreDraw' do
+pod 'CocoaLumberjack', '~> 3.2.0'
+pod 'SSZipArchive', '~> 1.8.1'
+end
---
+++ ZYP_CoreDraw/Base/ViewController/ZYPBaseViewController.h
@@ -0,0 +1,17 @@
+//
+//  ZYPBaseViewController.h
+//  ZYP_HomeController
+//
+//  Created by zhaoyunpeng on 2017/7/3.
+//  Copyright © 2017年 zhaoyunpeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZYPBaseViewController : UIViewController
+
+@property (nonatomic, strong) UIButton *button;
+
+- (void)onClickButton:(UIButton *)button;
+
+@end
